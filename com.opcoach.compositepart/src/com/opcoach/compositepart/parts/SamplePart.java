bf20b3e733408b5a5f13d26bd57ffd257215e0bb
Olivier Prouvost <olivier.prouvost@opcoach.com>
2020-10-23 15:18:10 +0200

    ajout de la gestion de l'affichage des trimbar pour les parts
    il faut un trimbarLayout et utiliser le
    IPresentationEngine.RENDERING_PARENT_KEY
    
    Signed-off-by: Olivier Prouvost <olivier.prouvost@opcoach.com>

--- com.opcoach.compositepart/src/com/opcoach/compositepart/parts/SamplePart.java
+++ com.opcoach.compositepart/src/com/opcoach/compositepart/parts/SamplePart.java
@@ -2,12 +2,20 @@ package com.opcoach.compositepart.parts;
 
 import java.util.Arrays;
 import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.Persist;
+import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.model.application.ui.basic.MTrimBar;
+import org.eclipse.e4.ui.workbench.IPresentationEngine;
+import org.eclipse.e4.ui.workbench.UIEvents;
+import org.eclipse.e4.ui.workbench.renderers.swt.TrimmedPartLayout;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
@@ -15,6 +23,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
+import org.osgi.service.event.Event;
 
 public class SamplePart  extends PartInCompositeWithTopToolbar {
 
@@ -22,27 +31,47 @@ public class SamplePart  extends PartInCompositeWithTopToolbar {
 
 	@Inject
 	private MPart part;
-
+	
 	@PostConstruct
 	public void createComposite(Composite parent) {
-		parent.setLayout(new GridLayout(1, false));
-
-		Text txtInput = new Text(parent, SWT.BORDER);
+		//parent.setLayout(new GridLayout(1, false));
+		TrimmedPartLayout layout = new TrimmedPartLayout(parent);
+		parent.setLayout(layout);
+		Composite centerComposite = layout.clientArea;
+		 centerComposite.setLayout(new GridLayout(1,false));
+		
+		
+		Text txtInput = new Text(centerComposite, SWT.BORDER);
 		txtInput.setMessage("Enter text to mark part as dirty");
 		txtInput.addModifyListener(e -> part.setDirty(true));
 		txtInput.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
-		tableViewer = new TableViewer(parent);
+		tableViewer = new TableViewer(centerComposite);
 
 		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
 		tableViewer.setInput(createInitialDataModel());
 		tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		MTrimBar trimbar = part.getTrimBars().get(0);
+		trimbar.getTransientData().put(IPresentationEngine.RENDERING_PARENT_KEY, parent);
+
 	}
+	
+	
+	@Inject
+	@Optional
+	private void showTrimbar(
+			@UIEventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) Event event, MApplication app) {
+		System.out.println("Showing trimbar");
+		//part.getTrimBars().get(0).setVisible(true);
+		part.getTrimBars().get(0).setToBeRendered(false);
+		part.getTrimBars().get(0).setToBeRendered(true);
+}
 
 	@Focus
 	public void setFocus() {
 		tableViewer.getTable().setFocus();
-	}
+	} 
 
 	@Persist
 	public void save() {
